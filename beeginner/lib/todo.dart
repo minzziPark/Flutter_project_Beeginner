70e7320b2f76f1c3b349b2578614fc8b544667e1
minzziPark <minzzi0068@gmail.com>
2023-11-28 05:42:35 +0900

    Todolist 보여주기 업데이트

--- beeginner/lib/todo.dart
+++ beeginner/lib/todo.dart
@@ -2,20 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:intl/intl.dart';
 import 'package:timer_builder/timer_builder.dart';
+import 'package:provider/provider.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'model/todolist.dart';
+import 'package:beeginner/main.dart';
 
 // import 'components/appbar.dart';
 // import 'components/getday.dart';
 
-class TodoPage extends StatefulWidget {
-  const TodoPage({super.key});
+class TodoPage extends StatelessWidget {
+  const TodoPage({Key? key}) : super(key: key);
 
-  @override
-  State<TodoPage> createState() => _TodoPageState();
-}
+  List<Card> _buildGridCards(BuildContext context, List<Todo> todos) {
+    var appState = context.watch<ApplicationState>();
+    if (todos.isEmpty) {
+      return const <Card>[];
+    }
 
-class _TodoPageState extends State<TodoPage> {
+    return todos.map((todo) {
+      return Card(
+        color: Colors.transparent,
+        elevation: 0,
+        child: Stack(children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        todo.todoTitle,
+                        maxLines: 1,
+                      ),
+                      const SizedBox(height: 8.0),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ]),
+      );
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
+    snapshots() => FirebaseFirestore.instance.collection('todo').snapshots();
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -138,9 +176,90 @@ class _TodoPageState extends State<TodoPage> {
               },
             ),
             const SizedBox(height: 13.0),
+            Container(
+              width: 380,
+              height: 500,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(210, 210, 210, 0.28), // 배경색 설정
+                borderRadius: BorderRadius.circular(10.0), // radius 설정
+              ),
+              child: StreamBuilder<QuerySnapshot>(
+                stream: snapshots(),
+                builder: _builder,
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _builder(BuildContext context, AsyncSnapshot snapshot) {
+    if (snapshot.data == null) return Container();
+
+    List<Todo> todos = snapshot.data?.docs.map<Todo>((data) {
+          Todo todo = Todo.fromJson(data.data() as Map<String, dynamic>);
+          todo.id = todo.id;
+          return todo;
+        }).toList() ??
+        [];
+
+    int numberOfTodos = todos.length;
+
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(24.0),
+          child: SizedBox(
+            width: 400,
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  const TextSpan(
+                    text: '오늘 총 ',
+                    style: TextStyle(
+                      color: Color(0xFF49454F),
+                      fontSize: 14,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w400,
+                      height: 1.0,
+                      letterSpacing: 0.25,
+                    ),
+                  ),
+                  TextSpan(
+                    text: '$numberOfTodos개', // 변수를 사용
+                    style: const TextStyle(
+                      color: Color(0xFF49454F),
+                      fontSize: 14,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w600,
+                      height: 1.0,
+                      letterSpacing: 0.25,
+                    ),
+                  ),
+                  const TextSpan(
+                    text: '의 할 일이 남아있어요!\n잊지않도록 도와줄게요 :)',
+                    style: TextStyle(
+                      color: Color(0xFF49454F),
+                      fontSize: 14,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w400,
+                      height: 1.0,
+                      letterSpacing: 0.25,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+            child: GridView.count(
+                crossAxisCount: 1,
+                padding: const EdgeInsets.all(11.0),
+                childAspectRatio: 6,
+                children: _buildGridCards(context, todos))),
+      ],
     );
   }
 }
