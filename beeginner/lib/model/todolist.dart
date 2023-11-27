0f59d76d655f8fed6db8652cf6249af9258e1bb3
minzziPark <minzzi0068@gmail.com>
2023-11-28 01:50:41 +0900

    Todo model 업데이트

---
+++ beeginner/lib/model/todolist.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Todo {
+  String? id;
+  late String todoTitle;
+  late bool checked;
+
+  Todo({
+    this.id,
+    required this.todoTitle,
+    required this.checked,
+  });
+
+  Todo.fromJson(Map<String, dynamic> json) {
+    fromJson(json);
+  }
+
+  void fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    todoTitle = json['todoTitle'];
+    checked = json['checked'];
+  }
+
+  Map<String, dynamic> toJson(Todo todo) => {
+        'id': id,
+        'todoTitle': todoTitle,
+        'checked': checked,
+      };
+}
