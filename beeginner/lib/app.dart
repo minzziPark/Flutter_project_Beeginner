07a583b76834551622b9420f4a8fb0ba5a2afaf8
minzziPark <minzzi0068@gmail.com>
2023-11-28 05:43:05 +0900

    login 됐을 때 todoPage로 넘어가게 하기

--- beeginner/lib/app.dart
+++ beeginner/lib/app.dart
@@ -1,19 +1,24 @@
+import 'package:beeginner/main.dart';
 import 'package:beeginner/schedule.dart';
 import 'package:beeginner/tip.dart';
 import 'package:beeginner/todo.dart';
 import 'package:beeginner/login.dart';
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 class BeeginnerApp extends StatelessWidget {
   const BeeginnerApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<ApplicationState>();
     return MaterialApp(
       title: 'Beeginner',
       initialRoute: '/login',
       routes: {
-        '/login': (BuildContext context) => const LoginPage(),
+        '/login': (BuildContext context) =>
+            !appState.loggedIn ? const LoginPage() : const TodoPage(),
         '/': (BuildContext context) => const TodoPage(),
         '/schedule': (BuildContext context) => const SchedulePage(),
         '/tip': (BuildContext context) => const TipPage(),
