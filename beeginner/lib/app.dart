c0b50af332d7215897bfc3ef4d58982dea33d3bf
minzziPark <minzzi0068@gmail.com>
2023-12-01 06:48:51 +0900

    페이지 이동 슬라이드 기능 제거

--- beeginner/lib/app.dart
+++ beeginner/lib/app.dart
@@ -1,10 +1,11 @@
 import 'package:beeginner/main.dart';
 import 'package:beeginner/schedule.dart';
 import 'package:beeginner/tip.dart';
+import 'package:beeginner/tip_write.dart';
 import 'package:beeginner/todo.dart';
 import 'package:beeginner/login.dart';
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class BeeginnerApp extends StatelessWidget {
@@ -16,12 +17,37 @@ class BeeginnerApp extends StatelessWidget {
     return MaterialApp(
       title: 'Beeginner',
       initialRoute: '/login',
-      routes: {
-        '/login': (BuildContext context) =>
-            !appState.loggedIn ? const LoginPage() : const TodoPage(),
-        '/': (BuildContext context) => const TodoPage(),
-        '/schedule': (BuildContext context) => const SchedulePage(),
-        '/tip': (BuildContext context) => const TipPage(),
+      onGenerateRoute: (settings) {
+        Widget page;
+        switch (settings.name) {
+          case '/login':
+            page = !appState.loggedIn ? const LoginPage() : const TodoPage();
+            break;
+          case '/':
+            page = const TodoPage();
+            break;
+          case '/schedule':
+            page = const SchedulePage();
+            break;
+          case '/tip':
+            page = const TipPage();
+            break;
+          case '/tip_write':
+            page = const TipWritePage();
+            break;
+          default:
+            page = const SizedBox(); // Handle unknown routes if needed
+        }
+
+        return PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => page,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            // Disable any transition
+            return child;
+          },
+          // Optional: Override the transition duration
+          transitionDuration: const Duration(milliseconds: 0),
+        );
       },
       theme: ThemeData.light(useMaterial3: true),
     );
