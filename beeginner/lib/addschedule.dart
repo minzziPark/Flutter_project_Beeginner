6dff6c832f2d9dcfd50cd7e25d3ce8f58d14e94d
minzziPark <minzzi0068@gmail.com>
2023-12-06 13:55:20 +0900

    달력 수정

--- beeginner/lib/addschedule.dart
+++ beeginner/lib/addschedule.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
+import 'package:timer_builder/timer_builder.dart';
 
 class AddSchedulePage extends StatefulWidget {
   const AddSchedulePage({super.key});
@@ -12,119 +14,279 @@ class _AddSchedulePageState extends State<AddSchedulePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
         backgroundColor: Colors.white,
-        automaticallyImplyLeading: false,
-        toolbarHeight: 100,
-        titleSpacing: 0,
-        title: Column(
-          children: [
-            Container(
-              width: 125,
-              child: SvgPicture.asset(
-                'assets/images/Beeginner.svg',
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          automaticallyImplyLeading: false,
+          toolbarHeight: 100,
+          titleSpacing: 0,
+          title: Column(
+            children: [
+              Container(
                 width: 125,
+                child: SvgPicture.asset(
+                  'assets/images/Beeginner.svg',
+                  width: 125,
+                ),
               ),
-            ),
-            Row(
-              children: <Widget>[
-                SizedBox(
-                  width: MediaQuery.of(context).size.width / 3,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Color.fromRGBO(
-                              230, 224, 233, 1), // 아래 border 색상 설정
-                          width: 0.1, // 아래 border 두께 설정
+              Row(
+                children: <Widget>[
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 3,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color.fromRGBO(
+                                230, 224, 233, 1), // 아래 border 색상 설정
+                            width: 0.1, // 아래 border 두께 설정
+                          ),
                         ),
                       ),
-                    ),
-                    child: TextButton(
-                      child: const Text(
-                        '오늘의 할 일',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/');
-                      },
-                      style: ButtonStyle(
-                        overlayColor: MaterialStateProperty.resolveWith<Color>(
-                          (Set<MaterialState> states) {
-                            return Colors.transparent;
-                          },
+                      child: TextButton(
+                        child: const Text(
+                          '오늘의 할 일',
+                          style: TextStyle(color: Colors.black),
                         ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width / 3, // 텍스트 버튼 폭 설정
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Color.fromRGBO(
-                              255, 226, 12, 1), // 아래 border 색상 설정
-                          width: 3.0, // 아래 border 두께 설정
-                        ),
-                      ),
-                    ),
-                    child: TextButton(
-                      child: const Text(
-                        '일정표',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      onPressed: () {},
-                      style: ButtonStyle(
-                        overlayColor: MaterialStateProperty.resolveWith<Color>(
-                          (Set<MaterialState> states) {
-                            return Colors.transparent;
-                          },
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/');
+                        },
+                        style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                              return Colors.transparent;
+                            },
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width / 3,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Color.fromRGBO(
-                              230, 224, 233, 1), // 아래 border 색상 설정
-                          width: 0.1, // 아래 border 두께 설정
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 3, // 텍스트 버튼 폭 설정
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color.fromRGBO(
+                                255, 226, 12, 1), // 아래 border 색상 설정
+                            width: 3.0, // 아래 border 두께 설정
+                          ),
                         ),
                       ),
-                    ),
-                    child: TextButton(
-                      child: const Text('꿀 팁',
-                          style: TextStyle(color: Colors.black)),
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/tip');
-                      },
-                      style: ButtonStyle(
-                        overlayColor: MaterialStateProperty.resolveWith<Color>(
-                          (Set<MaterialState> states) {
-                            return Colors.transparent;
-                          },
+                      child: TextButton(
+                        child: const Text(
+                          '일정표',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                              return Colors.transparent;
+                            },
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const Divider(
-              height: 0,
-              thickness: 0.5,
-              color: Color.fromRGBO(230, 224, 233, 1),
-            ),
-          ],
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 3,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color.fromRGBO(
+                                230, 224, 233, 1), // 아래 border 색상 설정
+                            width: 0.1, // 아래 border 두께 설정
+                          ),
+                        ),
+                      ),
+                      child: TextButton(
+                        child: const Text('꿀 팁',
+                            style: TextStyle(color: Colors.black)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/tip');
+                        },
+                        style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                              return Colors.transparent;
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Divider(
+                height: 0,
+                thickness: 0.5,
+                color: Color.fromRGBO(230, 224, 233, 1),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            children: [
+              TimerBuilder.periodic(
+                const Duration(seconds: 1),
+                builder: (context) {
+                  return Container(
+                    width: 242,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 226, 12, 1), // 배경색 설정
+                      borderRadius: BorderRadius.circular(10.0), // radius 설정
+                    ),
+                    padding: const EdgeInsets.all(0.0), // 내부 여백 설정
+                    child: Center(
+                      child: Text(
+                        'Today    |    ${DateFormat('yyyy.MM.dd.EEE').format(DateTime.now())}',
+                        style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black, // 텍스트 색상 설정
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(height: 13.0),
+              Container(
+                width: 380,
+                // height: 600,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(210, 210, 210, 0.28), // 배경색 설정
+                  borderRadius: BorderRadius.circular(10.0), // radius 설정
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(22.0, 23.0, 22.0, 0.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 35,
+                            child: SvgPicture.asset(
+                              'assets/images/tip_write_line.svg',
+                              height: 35,
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              // 버튼이 눌렸을 때의 동작
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.circular(10), // 버튼의 모양을 둥글게 설정
+                              ),
+                              primary: Color(0xFF696969), // 배경색 설정
+                            ),
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 15, horizontal: 20), // 내용의 패딩 설정
+                              child: Text(
+                                '날짜를 선택하세요.',
+                                style: TextStyle(
+                                  color: Color(0xFF696969),
+                                  fontSize: 15,
+                                  fontFamily: 'Roboto',
+                                  fontWeight: FontWeight.w300,
+                                  height: 0.06,
+                                  letterSpacing: 0.10,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 13),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(22.0, 0.0, 22.0, 0.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 35,
+                            child: SvgPicture.asset(
+                              'assets/images/tip_write_line.svg',
+                              height: 35,
+                            ),
+                          ),
+                          const Expanded(
+                            child: TextField(
+                              // controller: _titleController,
+                              decoration: InputDecoration(
+                                hintText: '총 근무시간을 선택하세요.',
+                                hintStyle: TextStyle(
+                                  color: Color(0xFF696969),
+                                  fontSize: 15,
+                                  fontFamily: 'Roboto',
+                                  fontWeight: FontWeight.w300,
+                                  height: 0.06,
+                                  letterSpacing: 0.10,
+                                ),
+                                counterText: '',
+                                border: InputBorder.none, // 입력 필드의 테두리 제거
+                                fillColor: Color.fromRGBO(
+                                    255, 255, 255, 0.5), // 배경색 제거
+                                filled: true, // 배경을 채우도록 설정
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(22.0, 0, 22, 0),
+                      child: Text(
+                        'MEMO',
+                        style: TextStyle(
+                          color: Color(0xFF696969),
+                          fontSize: 15,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w400,
+                          height: 0.11,
+                          letterSpacing: 0.10,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(10, 0, 10.0, 0),
+                      child: TextField(
+                        // controller: _descriptionController,
+                        maxLines: 18,
+                        decoration: const InputDecoration(
+                          hintText: '내용을 입력하세요.',
+                          hintStyle: TextStyle(
+                            color: Color(0xFF696969),
+                            fontSize: 13,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w300,
+                            height: 0.06,
+                            letterSpacing: 0.10,
+                          ),
+                          counterText: '',
+                          border: InputBorder.none, // 입력 필드의 테두리 제거
+                          fillColor: Color.fromARGB(0, 249, 149, 149), // 배경색 제거
+                          filled: true, // 배경을 채우도록 설정
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
