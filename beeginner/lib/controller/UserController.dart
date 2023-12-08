4dd98fd9b0e7096608a0cf4f33dabac44bf84d9a
minzziPark <minzzi0068@gmail.com>
2023-12-09 06:05:05 +0900

    user 컨트롤러 버그 수정

--- beeginner/lib/controller/UserController.dart
+++ beeginner/lib/controller/UserController.dart
@@ -4,8 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class UserController {
   static get collection => FirebaseFirestore.instance.collection('user');
 
-  static void add(User user) => collection.add(user.toJson(user));
-  static void update(User user) =>
+  static void add(Users user) => collection.add(user.toJson(user));
+  static void update(Users user) =>
       collection.doc(user.uid).set(user.toJson(user));
   static void delete(String id) => collection.doc(id).delete();
 }
